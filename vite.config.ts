a1a23f97089446f89fbe675f264540277aba9323
georgetoloraia <gtoloraia19@gmail.com>
2026-02-07 22:02:08 +0400

    chore: set pages base for production

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => ({
-  base: command === "build" ? "/selflink-community/" : "/",
+export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/selflink-community/" : "/",
   plugins: [react()],
 }));
